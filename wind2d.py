3f83746c7b3dcdc2e671ce5ee09bb561d8fbea7a
Hsiao Yao <tomtogo1260@gmail.com>
2025-07-07 11:26:06 +0800

    simple QApplication example

--- wind2d.py
+++ wind2d.py
@@ -1,3 +1,29 @@
+import sys
+from PyQt5.QtWidgets import (
+        QApplication,
+        QWidget,
+        QPushButton,
+        QVBoxLayout,
+        QMessageBox,
+        )
 
+def on_button_click():
+    QMessageBox.information(window, "message", "button click!")
+
+# python wind2d.py
 if __name__ == '__main__':
-    print('init')
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.setWindowTitle("Simple QApplication Example")
+
+    layout = QVBoxLayout()
+
+    button = QPushButton("click me")
+    button.clicked.connect(on_button_click)
+
+    layout.addWidget(button)
+    window.setLayout(layout)
+
+    window.show()
+    sys.exit(app.exec())
