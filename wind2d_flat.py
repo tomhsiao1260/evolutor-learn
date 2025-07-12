97fce2817ecbdeec93736f2f18af99a6ef00e013
Hsiao Yao <tomtogo1260@gmail.com>
2025-07-12 21:12:53 +0800

    show the input tif image on Qwidget

--- wind2d_flat.py
+++ wind2d_flat.py
@@ -1,5 +1,7 @@
 import sys
+import cv2
 import argparse
+import pathlib
 
 from PyQt5.QtWidgets import (
         QApplication,
@@ -8,6 +10,17 @@ from PyQt5.QtWidgets import (
         QMainWindow,
         QWidget,
         )
+from PyQt5.QtCore import (
+        QPoint,
+        Qt,
+        )
+from PyQt5.QtGui import (
+        QImage,
+        QPixmap,
+        )
+
+import numpy as np
+import cmap
 
 class MainWindow(QMainWindow):
 
@@ -19,6 +32,211 @@ class MainWindow(QMainWindow):
         widget = QWidget()
         widget.setLayout(grid)
         self.setCentralWidget(widget)
+        self.viewer = ImageViewer(self)
+        grid.addWidget(self.viewer, 0, 0)
+        self.viewer.setDefaults()
+
+        no_cache = parsed_args.no_cache
+        self.viewer.no_cache = no_cache
+        tifname = pathlib.Path(parsed_args.input_tif)
+        cache_dir = parsed_args.cache_dir
+        decimation = parsed_args.decimation
+        self.viewer.decimation = decimation
+        umbstr = parsed_args.umbilicus
+        window_width = parsed_args.window
+        maxrad = parsed_args.maxrad
+
+        if cache_dir is None:
+            cache_dir = tifname.parent
+        else:
+            cache_dir = pathlib.Path(cache_dir)
+        cache_file_base = cache_dir / tifname.stem
+        self.viewer.cache_dir = cache_dir
+        self.viewer.cache_file_base = cache_file_base
+        # nrrdname = cache_dir / (tifname.with_suffix(".nrrd")).name
+
+        print("loading tif", tifname)
+        self.viewer.loadTIFF(tifname)
+
+        self.viewer.overlay_maxrad = maxrad
+        self.viewer.warp_decimation = parsed_args.warp_decimation
+
+        self.viewer.drawAll()
+
+class ImageViewer(QLabel):
+
+    def __init__(self, main_window):
+        super(ImageViewer, self).__init__()
+        self.setMouseTracking(True)
+        self.main_window = main_window
+        self.image = None
+        self.zoom = 1.
+        self.center = (0,0)
+        self.bar0 = (0,0)
+        self.mouse_start_point = QPoint()
+        self.center_start_point = None
+        self.is_panning = False
+        self.dip_bars_visible = True
+        self.warp_dot_size = 3
+        self.umb = None
+        self.overlays = []
+        self.overlay_data = None
+        self.overlay_name = ""
+        self.decimation = 1
+        self.overlay_colormap = "viridis"
+        self.overlay_interpolation = "linear"
+        self.overlay_maxrad = None
+        self.overlay_alpha = None
+        self.overlay_scale = None
+        self.overlay_defaults = None
+        self.src_dots = None
+        self.dest_dots = None
+
+    def loadTIFF(self, fname):
+        try:
+            image = cv2.imread(str(fname), cv2.IMREAD_UNCHANGED).astype(np.float64)
+            image /= 65535.
+        except Exception as e:
+            print("Error while loading",fname,e)
+            return
+        self.image = image
+        self.image_mtime = fname.stat().st_mtime
+        self.setDefaults()
+        self.drawAll()
+
+    def setDefaults(self):
+        if self.image is None:
+            return
+        ww = self.width()
+        wh = self.height()
+        # print("ww,wh",ww,wh)
+        iw = self.image.shape[1]
+        ih = self.image.shape[0]
+        self.center = (iw//2, ih//2)
+        zw = ww/iw
+        zh = wh/ih
+        zoom = min(zw, zh)
+        self.setZoom(zoom)
+        print("center",self.center[0],self.center[1],"zoom",self.zoom)
+
+    def setZoom(self, zoom):
+        # TODO: set min, max zoom
+        prev = self.zoom
+        self.zoom = zoom
+        if prev != 0:
+            bw,bh = self.bar0
+            cw,ch = self.center
+            bw -= cw
+            bh -= ch
+            bw /= zoom/prev
+            bh /= zoom/prev
+            self.bar0 = (bw+cw, bh+ch)
+
+    # class function
+    def rectIntersection(ra, rb):
+        (ax1, ay1, ax2, ay2) = ra
+        (bx1, by1, bx2, by2) = rb
+        # print(ra, rb)
+        x1 = max(min(ax1, ax2), min(bx1, bx2))
+        y1 = max(min(ay1, ay2), min(by1, by2))
+        x2 = min(max(ax1, ax2), max(bx1, bx2))
+        y2 = min(max(ay1, ay2), max(by1, by2))
+        if (x1<x2) and (y1<y2):
+            r = (x1, y1, x2, y2)
+            # print(r)
+            return r
+
+    # input: 2D float array, range 0.0 to 1.0
+    # output: RGB array, uint8, with colors determined by the
+    # colormap and alpha, zoomed in based on the current
+    # window size, center, and zoom factor
+    def dataToZoomedRGB(self, data, alpha=1., colormap="gray", interpolation="linear", scale=1.):
+        if scale is None:
+            scale = 1.
+        if colormap in self.colormaps:
+            colormap = self.colormaps[colormap]
+        cm = cmap.Colormap(colormap, interpolation=interpolation)
+
+        iw = data.shape[1]
+        ih = data.shape[0]
+        z = self.zoom / scale
+        # zoomed image width, height:
+        ziw = max(int(z*iw), 1)
+        zih = max(int(z*ih), 1)
+        # viewing window width, height:
+        ww = self.width()
+        wh = self.height()
+        # print("di ww,wh",ww,wh)
+        # viewing window half width
+        whw = ww//2
+        whh = wh//2
+        cx,cy = self.center
+        cx *= scale
+        cy *= scale
+
+        # Pasting zoomed data slice into viewing-area array, taking
+        # panning into account.
+        # Need to calculate the interesection
+        # of the two rectangles: 1) the panned and zoomed slice, and 2) the
+        # viewing window, before pasting
+        ax1 = int(whw-z*cx)
+        ay1 = int(whh-z*cy)
+        ax2 = ax1+ziw
+        ay2 = ay1+zih
+        bx1 = 0
+        by1 = 0
+        bx2 = ww
+        by2 = wh
+        ri = ImageViewer.rectIntersection((ax1,ay1,ax2,ay2), (bx1,by1,bx2,by2))
+        outrgb = np.zeros((wh,ww,3), dtype=np.uint8)
+        if ri is not None:
+            (x1,y1,x2,y2) = ri
+            # zoomed data slice
+            x1s = int((x1-ax1)/z)
+            y1s = int((y1-ay1)/z)
+            x2s = int((x2-ax1)/z)
+            y2s = int((y2-ay1)/z)
+            zslc = cv2.resize(data[y1s:y2s,x1s:x2s], (x2-x1, y2-y1), interpolation=cv2.INTER_NEAREST)
+            cslc = cm(np.remainder(zslc, 1.))
+            outrgb[y1:y2, x1:x2, :] = (255*cslc[:,:,:3]*alpha).astype(np.uint8)
+        return outrgb
+
+    def drawAll(self):
+        if self.image is None:
+            return
+
+        total_alpha = .8
+        main_alpha = total_alpha
+
+        outrgb = self.dataToZoomedRGB(self.image, alpha=main_alpha)
+
+        ww = self.width()
+        wh = self.height()
+
+        scale = 1.
+
+        bytesperline = 3*outrgb.shape[1]
+        # print(outrgb.shape, outrgb.dtype)
+        qimg = QImage(outrgb, outrgb.shape[1], outrgb.shape[0],
+                      bytesperline, QImage.Format_RGB888)
+        # print("created qimg")
+        pixmap = QPixmap.fromImage(qimg)
+        # print("created pixmap")
+        self.setPixmap(pixmap)
+        # print("set pixmap")
+
+    colormaps = {
+            "gray": "matlab:gray",
+            "viridis": "bids:viridis",
+            "bwr": "matplotlib:bwr",
+            "cool": "matlab:cool",
+            "bmr_3c": "chrisluts:bmr_3c",
+            "rainbow": "gnuplot:rainbow",
+            "spec11": "colorbrewer:Spectral_11",
+            "set12": "colorbrewer:Set3_12",
+            "tab20": "seaborn:tab20",
+            "hsv": "matlab:hsv",
+            }
 
 class Tinter():
 
@@ -87,5 +305,6 @@ if __name__ == '__main__':
 
     tinter = Tinter(app, parsed_args)
     sys.exit(app.exec())
+
 
 
